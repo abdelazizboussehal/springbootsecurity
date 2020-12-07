5beaa7e9e7ee3a7a25bc424d76cb46cdc37bde73
abdelaziz.boussehal <abdelaziz.boussehal@univ-constantine2.dz>
2020-12-07 10:57:24 +0100

    version database extern and jasper file

--- src/main/java/com/example/demo/cours/CoursController.java
+++ src/main/java/com/example/demo/cours/CoursController.java
@@ -1,6 +1,6 @@
 package com.example.demo.cours;
 
-import com.example.demo.security.ReportService;
+import com.example.demo.services.ReportService;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/example/demo/services/ReportService.java
+++ src/main/java/com/example/demo/services/ReportService.java
@@ -1,4 +1,4 @@
-package com.example.demo.security;
+package com.example.demo.services;
 import com.example.demo.cours.Cour;
 import com.example.demo.cours.CrudCour;
 import net.sf.jasperreports.engine.*;
