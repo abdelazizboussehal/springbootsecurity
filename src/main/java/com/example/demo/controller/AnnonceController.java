53236a3b96140651373cb8dceb528ddaeb7d74cf
abdelaziz.boussehal <abdelaziz.boussehal@univ-constantine2.dz>
2020-12-09 18:55:34 +0100

    version database extern and jasper file with jwt authentification gestion des annonces project with  custom exception handling

--- src/main/java/com/example/demo/controller/AnnonceController.java
+++ src/main/java/com/example/demo/controller/AnnonceController.java
@@ -1,23 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.exeptions.ResourceNotFoundException;
 import com.example.demo.modeles.annonce.Annonce;
 import com.example.demo.repositories.annonce.CrudAnnonce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@ResponseBody
 public class AnnonceController {
     @Autowired
     CrudAnnonce crudAnnonce;
 
-    @RequestMapping("/annonce/")
-    public List<Annonce> annonceList(){
+    @RequestMapping("/annonce")
+    public  List<Annonce> annonceList(){
         return (List<Annonce>) crudAnnonce.findAll();
+    }
+
+    @RequestMapping("/annonce/{id}")
+    public  Annonce annonce(@PathVariable int id)  {
+        return (Annonce) crudAnnonce
+                .findById(id)
+                .orElseThrow(()->new ResourceNotFoundException(
+                        "annonce with this: "+id+" id not found"));
     }
 
     @RequestMapping(method = RequestMethod.POST,value = "/annonce/")
